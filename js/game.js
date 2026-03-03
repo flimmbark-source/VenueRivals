e215486163fb8dfee4ddef108f114b6cc151b4ab
flimmbark-source <flimmbark@gmail.com>
2026-03-03 19:38:47 +0200

    Remove unintended end-of-grid exit slot

--- js/game.js
+++ js/game.js
@@ -790,7 +790,7 @@ const Game = (() => {
   }
 
   function getHouseCapacity(venue) {
-    return Math.max(0, (venue?.gridSize || 0) - 1);
+    return Math.max(0, (venue?.gridSize || 0) - 2);
   }
 
   function createPlayer(name, venueId, isAI) {
